e9b8e234e3cdb010d9b7c17a1722fea739f8e8b5
jurieo <jurieo@jurieo.com>
2024-02-28 18:46:03 +0800

    feat: 部署之前需配置重要信息

--- quick-install.sh
+++ quick-install.sh
@@ -8,12 +8,14 @@ git clone https://github.com/jurieo/chatgpt-share-web.git chatgpt-share-web
 ## 进入目录
 cd chatgpt-share-web
 
-docker compose pull
-docker compose up -d --remove-orphans
+# docker compose pull
+# docker compose up -d --remove-orphans
 
 ## 提示信息
-echo "服务启动成功，请访问 http://localhost:38300"
-echo "管理员后台地址 http://localhost:38300/xyhelper"
+echo "请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,然后执行以下命令"
+echo "./deploy.sh"
+echo "服务启动成功后，请访问://localhost:38300"
+echo "管理员后台地址:http://localhost:38300/xyhelper"
 echo "管理员账号: admin"
 echo "管理员密码: 123456"
 echo "请及时修改管理员密码"
