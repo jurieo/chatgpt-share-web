ab9ca41c5902ca8ed6ac58ed23cbe9cf9ff2a4dd
jurieo <jurieo@jurieo.com>
2024-03-13 12:52:18 +0800

    docs: 更新后台路径

--- quick-install.sh
+++ quick-install.sh
@@ -17,7 +17,7 @@ chmod +x ./deploy.sh
 echo "请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,然后执行以下命令"
 echo "./deploy.sh"
 echo "服务启动成功后，请访问://localhost:38300"
-echo "管理员后台地址:http://localhost:38300/xyhelper"
+echo "管理员后台地址:http://localhost:38300/shareadmin"
 echo "管理员账号: admin"
 echo "管理员密码: 123456"
 echo "请及时修改管理员密码"
