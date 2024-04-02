8099f354a9eaeefc2f966fa4bf7f83bd601c7e02
jurieo <jurieo@jurieo.com>
2024-04-02 15:51:10 +0800

    docs: update

--- quick-install.sh
+++ quick-install.sh
@@ -20,8 +20,8 @@ local_ipv4=$(curl -s4m8 http://ip.sb)
 ## 提示信息
 echo "请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,然后执行以下命令"
 echo "./deploy.sh"
-echo "服务启动成功后，请访问://${local_ipv4}:38300"
-echo "管理员后台地址:http://${local_ipv4}:38300/shareadmin"
+echo "服务启动成功后，请访问: http://${local_ipv4}:38300"
+echo "管理员后台地址: http://${local_ipv4}:38300/shareadmin"
 echo "管理员账号: admin"
 echo "管理员密码: 123456"
 echo "请及时修改管理员密码"
