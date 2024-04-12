2d3c8a4f7e4df68c8119b2063a3f862f42f1275d
jurieo <jurieo@jurieo.com>
2024-04-12 11:14:25 +0800

    docs: update

--- quick-install.sh
+++ quick-install.sh
@@ -18,11 +18,15 @@ docker compose pull
 docker compose up -d --remove-orphans
 
 ## 提示信息
-echo "服务启动成功后，请访问: http://${local_ipv4}:38300"
+echo "=================感谢您的耐心等待，部署已完成！=================== "
+echo ""
+echo "请访问: http://${local_ipv4}:38300 开始使用！"
 echo "管理员后台地址: http://${local_ipv4}:38300/shareadmin"
-echo "管理员账号: admin"
-echo "管理员密码: 123456"
-echo "请及时修改管理员密码"
-echo "现在，还有有很多个性化的配置，你可以根据实际情况进行设置。请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,保存后执行以下命令"
+echo "后台管理员账号/密码: 【admin/123456】，请及时修改管理员密码"
+echo "现在，还有有很多个性化的配置，你可以根据实际情况进行设置。"
+echo "请进入chatgpt-share-web目录 (cd chatgpt-share-web),修改config.yaml文件中的重要信息,保存后执行以下命令"
 echo "./restart.sh"
+echo "通过反代服务器的38300端口，即可使用域名访问您的服务。"
 echo "有任何问题请加入TG群: https://t.me/chatgpt_share_web"
+echo ""
+echo "====================================================== "
